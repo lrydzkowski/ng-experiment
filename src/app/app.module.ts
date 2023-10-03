a62685a5392e2c6ca349f1a53a885b56dd7f0613
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-10-03 20:17:18 +0200

    feat: add test component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsListComponent } from './features/catalog/components/products-list/products-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ProductsListComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/features/catalog/components/products-list/products-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'catalog-products-list',
+  templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css'],
+})
+export class ProductsListComponent {}
+
