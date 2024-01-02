dcb1915a42a8a49f279b7bd3ee75f0d5b2275471
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-01-02 12:57:51 +0100

    feat: introduce NgOptimizedImage directive

--- src/app/features/core/components/top-bar/top-bar.component.ts
+++ src/app/features/core/components/top-bar/top-bar.component.ts
@@ -4,12 +4,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'core-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css'],
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, RouterLink],
+  imports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, RouterLink, NgOptimizedImage],
 })
 export class TopBarComponent {}
