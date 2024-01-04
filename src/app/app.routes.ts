72fe79bbccffcba8ba780c11d0a526cc5c7b3362
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-01-04 18:26:35 +0100

    feat: add not found page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomePageComponent } from './features/core/pages/home-page/home-page.com
 import { ProductsListPageComponent } from './features/catalog/pages/products-list-page/products-list-page.component';
 import { WebsocketNotificationsPageComponent } from './features/notifications/pages/websocket-notifications-page/websocket-notifications-page.component';
 import { SignalrNotificationsPageComponent } from './features/notifications/pages/signalr-notifications-page/signalr-notifications-page.component';
+import { NotFoundPageComponent } from './features/core/pages/not-found-page/not-found-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'products', component: ProductsListPageComponent },
   { path: 'websocket-notifications', component: WebsocketNotificationsPageComponent },
   { path: 'signalr-notifications', component: SignalrNotificationsPageComponent },
+  { path: '**', component: NotFoundPageComponent },
 ];
---
+++ src/app/features/core/pages/not-found-page/not-found-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'core-not-found-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './not-found-page.component.html',
+  styleUrl: './not-found-page.component.css',
+})
+export class NotFoundPageComponent {}
+
