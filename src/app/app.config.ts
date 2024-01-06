558b6a1eeee96e01eba0342f696981acb54421fb
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-01-06 21:32:30 +0100

    feat: introduce SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations()],
+  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations(), provideClientHydration()],
 };
