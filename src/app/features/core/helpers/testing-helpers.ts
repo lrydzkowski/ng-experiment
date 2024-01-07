58d8300577d2c2220126533310202a9aef38a99d
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-01-07 21:55:08 +0100

    feat: extend helpers for testing

--- src/app/features/core/helpers/testing-helpers.ts
+++ src/app/features/core/helpers/testing-helpers.ts
@@ -1,3 +1,4 @@
+import { DebugElement } from '@angular/core';
 import { defer } from 'rxjs';
 
 export function asyncData<T>(data: T) {
@@ -6,4 +7,17 @@ export function asyncData<T>(data: T) {
 
 export function asyncError<T>(errorObject: T) {
   return defer(() => Promise.reject(errorObject));
+}
+
+export const ButtonClickEvents = {
+  left: { button: 0 },
+  right: { button: 2 },
+};
+
+export function click(el: DebugElement | HTMLElement, eventObj: unknown = ButtonClickEvents.left): void {
+  if (el instanceof HTMLElement) {
+    el.click();
+  } else {
+    el.triggerEventHandler('click', eventObj);
+  }
 }
