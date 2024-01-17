e4e44bd927c1b91e1072931639e6df94b730adf3
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2024-01-17 19:12:09 +0100

    feat: introduce small visual adjustments

--- src/app/features/core/components/top-bar/top-bar.component.ts
+++ src/app/features/core/components/top-bar/top-bar.component.ts
@@ -4,13 +4,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'core-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.css'],
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, RouterLink, NgOptimizedImage],
+  imports: [MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, RouterLink],
 })
 export class TopBarComponent {}
